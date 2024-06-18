ca48c041b5b68dbe552bf65eaa7191478fdeb626
Aidana Turdueva <ajdanaturdueva9@gmail.com>
2024-06-18 14:29:53 +0300

    home-work-5

--- lection_5/homework.js
+++ lection_5/homework.js
@@ -11,23 +11,33 @@
 // console.log(c.z); // ?
 // c.z = 3;
 // console.log(a.z); // ?
+//?1решение)
+// console.log(c.b); // 1
+// delete c.b;
+// console.log(c.b); // 1
+// delete a.b;
+// console.log(c.b); // undefined
+// a.z = 2;
+// console.log(c.z); // 2
+// c.z = 3;
+// console.log(a.z); // 2
 
 // 2.
 
 // const promise = new Promise(() => {
 // })
-// promise.prototype === Promise.__proto__ // ?
+// promise.prototype === Promise.__proto__ // ?  false
 //
 // const obj = {}
-// obj.__proto__ === Object.prototype // ?
+// obj.__proto__ === Object.prototype // ? true
 //
-// new Array([]).__proto__ === Array.prototype // ?
+// new Array([]).__proto__ === Array.prototype // ? true
 //
 // function Fn1 () {}
 // function Fn2 () {}
-// Fn1.constructor === Fn2.constructor // ?
+// Fn1.constructor === Fn2.constructor // ? true
 //
-// Fn1.prototype === Fn2.prototype // ?
+// Fn1.prototype === Fn2.prototype // ? false
 //3.
 
 // У вас есть два конструктора, Animal и Bird.
@@ -38,7 +48,7 @@
 // Добавьте метод speak в Animal, который выводит в консоль звук, издаваемый животным (например, "Some generic sound").
 // Создайте конструктор Bird, который принимает параметр name и вызывает конструктор Animal с тем же именем.
 // Добавьте метод fly в Bird, который выводит в консоль сообщение о том, что птица летит (например, "Flying high!").
-// Создайте объекты animal и bird с использованием соответствующих конструкторов и вызовите их методы speak и fly.
+// Создайте объекты animal и bird с исполь  зованием соответствующих конструкторов и вызовите их методы speak и fly.
 // Решите задачу, используя прототипное наследование, чтобы Bird наследовал от Animal.
 
 // Должно быть такое поведение:
@@ -48,5 +58,3 @@
 // animal.speak(); // "Some generic sound"
 // bird.speak();   // "Some generic sound"
 // bird.fly();     // "Flying high!"
-
-
